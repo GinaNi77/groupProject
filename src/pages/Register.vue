58e014b00cc0a370c3a50ee134e471def35c5086
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-13 21:32:14 +0300

    Добавила страницу Register для авторизации пользователей, настроила роутеры для перехода на стр Register

---
+++ src/pages/Register.vue
@@ -0,0 +1,46 @@
+<template>
+    <q-page padding>
+        <q-form class="row justify-center" @submit.prevent="">
+            <p class="col-12 text-h5 text-center">Register</p>
+            <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-lg">
+                <q-input
+                    label="Name"
+                    v-model="form.name"
+                />
+
+                <q-input
+                    label="Email"
+                    v-model="form.email"
+                />
+
+                <q-input
+                    label="Password"
+                    v-model="form.password"
+                /> 
+
+                <div class="q-mt-lg">
+                    <q-btn outline size="md" color="black" label="Register" class="full-width" type="submit"/>     
+                </div>    
+            </div>
+        </q-form>
+    </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'PageRegister',
+  setup(){
+    const form = ref({
+        name:"",
+        email: "",
+        password: ""
+    })
+
+    return{
+        form
+    }
+  }
+})
+</script>
