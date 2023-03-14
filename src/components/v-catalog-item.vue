88208af7336785763dd6e7c16c1d086fb68076b8
SofiyaKH <119045257+SofiyaKH@users.noreply.github.com>
2023-03-14 14:36:30 +0300

    Реализован компонент v-catalog-item, реализована первичная верстка страницы Catalog

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius">
+    <img src="../assets/3.png" />
+
+    <q-card-section class="q-pa-xs">
+      <div class="text-weight-light">Name</div>
+      <div class="text-weight-bold">24$</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "v-catalog-item",
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.v-catalog-item {
+  max-width: 250px;
+  max-height: 464px;
+}
+</style>
---
+++ src/pages/Catalog.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page class="flex flex-start q-pt-xl">
+    <div class="v-catalog flex flex-start q-px-xl">
+      <vCatalogItem v-for="item in items" :key="item.id" />
+    </div>
+    <div class="v-menu">
+      <div class="v-gender">
+        <ul>
+          <li>Woman</li>
+          <li>Man</li>
+          <li>Kids</li>
+        </ul>
+      </div>
+      <div class="v-attr">
+        <ul>
+          <li>Size</li>
+          <li>Price</li>
+        </ul>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import vCatalogItem from "../components/v-catalog-item.vue";
+
+const items = [
+  {
+    id: 1,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+];
+</script>
+
+<style scoped>
+.v-catalog {
+  max-width: 900px;
+}
+.v-menu {
+  max-width: 350px;
+  width: 100%;
+}
+</style>
