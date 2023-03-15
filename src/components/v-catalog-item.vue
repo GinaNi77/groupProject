dc6cdfdd52065253403e8243d9408b923f39b1ed
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-16 00:21:37 +0300

    удачный вывод даныхх из бд в каталог=>элементы каталога

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,15 +1,5 @@
 <template>
-  <!-- <q-card class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius">
-    <img src="../assets/3.png"/>
-    <q-card-section class="q-pa-xs">
-      <div class="text-weight-light">{{item_data.title}}</div>
-      <div class="text-weight-bold">{{item_data.price}}</div>
-    </q-card-section>
-  </q-card> -->
-
-<div v-if="loading" class="flex flex-center">Loading...</div>
-<div v-else-if="error">Error: {{ error.message }}</div>
-  <q-card v-else-if="result && result.products" class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius" v-for="product in result.products"  :key="product.id">  
+  <q-card class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius">  
     <img :src=" require('../assets/images/' + product.img)"/>
     <q-card-section class="q-pa-xs">
       <div class="text-weight-light">{{product.description}}</div>
@@ -22,50 +12,24 @@
 <script>
 
 import { defineComponent } from 'vue'
-import { useQuery } from '@vue/apollo-composable'
-import gql from 'graphql-tag'
-import { watch } from 'vue'
 
 export default defineComponent ({
 
+  name: "v-catalog-item",
+  props: {
+        product:{
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+  },
+  setup(props) {
+    console.log(props.product) 
 
-//   name: "v-catalog-item",
-//   props: {
-//         item_data:{
-//             type: Object,
-//             default() {
-//                 return {}
-//             }
-//         }
-//   },
-  
+    return {
 
-
-  setup() {
-    const {result, loading, error} = useQuery(gql`
-        query MyQuery {
-          products {
-            id
-            description
-            price
-            img
-            title
-      }
-      }`)
-
-      // const {result, loading, error} = useQuery(gql`
-      //   query MyQuery {
-      //   products(where: {sex: {_eq: "F"}}) {
-      //     id
-      //     title
-      //     img
-      //     description
-      //     price
-
-      //   }
-      // }`)
-
-    return { result, loading, error};
+    };
   }
 })
 
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="flex justify-center q-px-xl">
     <div class="v-catalog flex flex-start">
-      <vCatalogItem v-for="item in items" :key="item.id" />
+      <vCatalogItem v-for="product in products" :key="product.id" :product="product"/>
     </div>
     <div class="v-menu q-pl-sm">
       <div class="v-gender q-pb-lg">
         <q-list>
-          <q-item clickable dense v-ripple>
+          <q-item clickable dense v-ripple >
             <q-item-section class="text-weight-bold">Woman</q-item-section>
           </q-item>
           <q-item clickable dense v-ripple>
@@ -17,6 +17,7 @@
           </q-item>
         </q-list>
       </div>
+
       <div class="v-attr q-pt-lg">
         <q-list>
           <q-item clickable dense v-ripple>
@@ -37,23 +38,38 @@
   </q-page>
 </template>
 
-<script setup>
-import vCatalogItem from "../components/v-catalog-item.vue";
+<script>
+import vCatalogItem from "src/components/v-catalog-item.vue";
 
-const items = [
-  {
-    id: 1,
+import { defineComponent } from 'vue'
+import { computed } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+export default defineComponent ({
+  name: "v-catalog-item",
+
+  components: {
+        vCatalogItem
+    },
+
+  setup() {
+    const {result} = useQuery(gql`
+        query MyQuery {
+          products {
+            id
+            description
+            price
+            img
+      }
+      }`)
+
+      const products = computed(() => result.value?.products ?? [])
+
+    return {products};
   },
-  {
-    id: 3,
-  },
-  {
-    id: 4,
-  },
-  {
-    id: 5,
-  },
-];
+})
+
 </script>
 
 <style scoped>
