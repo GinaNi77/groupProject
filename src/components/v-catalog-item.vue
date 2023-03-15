103af121b5502eaa97f51d9dc2e688fc2ac50904
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-16 01:22:37 +0300

    добавили кнопку для определения id выбранного товара

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,10 +1,11 @@
 <template>
-  <q-card class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius">  
+  <q-card class="my-card q-mx-sm q-my-md v-catalog-item no-box-shadow no-border-radius">  
     <img :src=" require('../assets/images/' + product.img)"/>
     <q-card-section class="q-pa-xs">
       <div class="text-weight-light">{{product.description}}</div>
       <div class="text-weight-bold">{{product.price}}</div>
       <div class="text-weight-bold">{{product.title}}</div>
+      <q-btn class="flat q-mt-md full-width" outline @click="loadData(product.id)">buy</q-btn>
     </q-card-section>
   </q-card>
 </template>
@@ -25,11 +26,15 @@ export default defineComponent ({
         }
   },
   setup() {
-  
-    return {
-
-    };
+    const loadData = (id) => {
+    console.log(id)
   }
+
+    return {
+      loadData
+    }
+  
+}
 })
 
 </script>
