6468e5181e2dac3bebbe0fde744d57fd0ddb3965
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-16 00:08:13 +0300

    новый католог айтем

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,22 +1,74 @@
 <template>
-  <q-card class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius">
-    <img src="../assets/3.png" />
-
+  <!-- <q-card class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius">
+    <img src="../assets/3.png"/>
     <q-card-section class="q-pa-xs">
-      <div class="text-weight-light">Name</div>
-      <div class="text-weight-bold">24$</div>
+      <div class="text-weight-light">{{item_data.title}}</div>
+      <div class="text-weight-bold">{{item_data.price}}</div>
+    </q-card-section>
+  </q-card> -->
+
+<div v-if="loading" class="flex flex-center">Loading...</div>
+<div v-else-if="error">Error: {{ error.message }}</div>
+  <q-card v-else-if="result && result.products" class="my-card q-ma-sm v-catalog-item no-box-shadow no-border-radius" v-for="product in result.products"  :key="product.id">  
+    <img :src=" require('../assets/images/' + product.img)"/>
+    <q-card-section class="q-pa-xs">
+      <div class="text-weight-light">{{product.description}}</div>
+      <div class="text-weight-bold">{{product.price}}</div>
+      <div class="text-weight-bold">{{product.title}}</div>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
-export default {
-  name: "v-catalog-item",
-  props: {},
-  data() {
-    return {};
-  },
-};
+
+import { defineComponent } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { watch } from 'vue'
+
+export default defineComponent ({
+
+
+//   name: "v-catalog-item",
+//   props: {
+//         item_data:{
+//             type: Object,
+//             default() {
+//                 return {}
+//             }
+//         }
+//   },
+  
+
+
+  setup() {
+    const {result, loading, error} = useQuery(gql`
+        query MyQuery {
+          products {
+            id
+            description
+            price
+            img
+            title
+      }
+      }`)
+
+      // const {result, loading, error} = useQuery(gql`
+      //   query MyQuery {
+      //   products(where: {sex: {_eq: "F"}}) {
+      //     id
+      //     title
+      //     img
+      //     description
+      //     price
+
+      //   }
+      // }`)
+
+    return { result, loading, error};
+  }
+})
+
 </script>
 
 <style>
@@ -24,4 +76,5 @@ export default {
   max-width: 250px;
   max-height: 464px;
 }
+
 </style>
