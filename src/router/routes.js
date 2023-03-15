a93c89cb4c4fb9b0c8081151fe81e264c8641caa
SofiyaKH <119045257+SofiyaKH@users.noreply.github.com>
2023-03-15 15:52:49 +0300

    Реализована страница товара

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,11 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', name:'loginDefault', component: () => import('pages/Login.vue') },
-      { path: 'login', name:'login', component: () => import('pages/Login.vue') },
-      { path: 'register', name:'register', component: () => import('pages/Register.vue') },
+      { path: '', name: 'loginDefault', component: () => import('pages/Login.vue') },
+      { path: 'login', name: 'login', component: () => import('pages/Login.vue') },
+      { path: 'register', name: 'register', component: () => import('pages/Register.vue') },
       { path: 'catalog', component: () => import('pages/Catalog.vue') },
+      { path: 'product', component: () => import('pages/Product.vue') },
       { path: 'cart', component: () => import('components/Cart.vue') },
     ]
   },
