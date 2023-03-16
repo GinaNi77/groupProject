39e8fbbc4a0fab98e5c7b94ece81d76ec67f4108
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-16 12:45:13 +0300

    новый компонент ProductPopup

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,10 +1,20 @@
 <template>
   <q-card class="my-card q-mx-sm q-my-lg v-catalog-item no-box-shadow no-border-radius">  
+
+    <vProductPopup v-if="isProductPopupVisible"/>
+
     <img :src=" require('../assets/images/' + product.img)"/>
     <q-card-section class="q-pa-xs">
       <div class="text-weight-light">{{product.description}}</div>
       <div class="text-weight-bold">{{product.price}}</div>
-      <q-btn class="flat q-mb-sm text-black full-width" size="sm" :ripple="false" flat @click="loadData(product.id)">learn more</q-btn>
+
+      <q-btn class="flat q-mb-sm text-black full-width" 
+        size="sm" 
+        :ripple="false" 
+        flat @click="showProductPopup">
+        learn more
+      </q-btn>
+
       <q-btn class="flat full-width" outline @click="loadData(product.id)">buy</q-btn>
       
     </q-card-section>
@@ -13,11 +23,18 @@
 
 <script>
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import {vProductPopup} from "src/components/v-product-popup.vue"
+
 
 export default defineComponent ({
 
   name: "v-catalog-item",
+  components:{
+    vProductPopup
+
+  },
+
   props: {
         product:{
             type: Object,
@@ -26,13 +43,21 @@ export default defineComponent ({
             }
         }
   },
+
   setup() {
+
+    const isProductPopupVisible = ref(false)
+
     const loadData = (id) => {
-    console.log(id)
-  }
+      console.log(id)
+    }
+
+    const showProductPopup = ()=>{
+      isProductPopupVisible.value = true
+    }
 
     return {
-      loadData
+      loadData, isProductPopupVisible, showProductPopup
     }
   
 }
