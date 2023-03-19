ff84489a2074bd8abdec292a95c17cc60cda8c2f
Nigina Gafurova <nigina.gafurova.2001@mail.ru>
2023-03-19 12:30:20 +0300

    registration/login

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', name: 'loginDefault', component: () => import('pages/Login.vue') },
-      { path: 'login', name: 'login', component: () => import('pages/Login.vue') },
-      { path: 'register', name: 'register', component: () => import('pages/Register.vue') },
+      { path: '', name: 'catalogDefault', component: () => import('pages/Catalog.vue') },
+      // { path: 'login', name: 'login', component: () => import('pages/Login.vue') },
+      // { path: 'register', name: 'register', component: () => import('pages/Register.vue') },
       { path: 'catalog', component: () => import('pages/Catalog.vue') },
       { path: 'product/:id', component: () => import('pages/ProductCard.vue'), props:true },
       { path: 'cart', component: () => import('components/Cart.vue') },
