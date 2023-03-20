4dbf980dd2555c99c4b4eec95b88f695df7a40a5
firttrule <vanezik23@mail.ru>
2023-03-20 20:47:31 +0300

    Добавление товаров в корзину в реальном времени | 1.2

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -8,12 +8,12 @@
     </div>
     <div v-else-if="carts">
       <div v-if="carts.length">
-        <CartItem
+        <!-- <CartItem
           v-for="(item, index) in items"
           :key="item.id"
           :carts="item"
           @deleteFromCarts="deleteFromCarts(index)"
-        />
+        /> -->
         <div class="q-ma-md flex justify-end">
           <q-btn
             @click="deleteAllFromCarts"
@@ -83,54 +83,9 @@ export default defineComponent({
     onResult(() => {
       items.value = result.value.carts;
     });
-    
+    console.log(result);
+
     return { result, items, carts, loading, error };
-  },
-  data() {
-    return {
-      // cart: [
-      //   {
-      //     id: 1,
-      //     image: "vest.svg",
-      //     description: "WAISTCOAT WITH CONTRAST PIPING",
-      //     color: "Navy Blue",
-      //     size: "M",
-      //     units: 1,
-      //     amount: 24,
-      //     ref: "3123/619",
-      //   },
-      //   {
-      //     id: 2,
-      //     image: "blouse.svg",
-      //     description: "WAISTCOAT WITH CONTRAST PIPING",
-      //     color: "Navy Blue",
-      //     size: "M",
-      //     units: 1,
-      //     amount: 24,
-      //     ref: "3123/619",
-      //   },
-      //   {
-      //     id: 3,
-      //     image: "boots.svg",
-      //     description: "WAISTCOAT WITH CONTRAST PIPING",
-      //     color: "Navy Blue",
-      //     size: "XL",
-      //     units: 1,
-      //     amount: 24,
-      //     ref: "3123/619",
-      //   },
-      //   {
-      //     id: 4,
-      //     image: "shirt.svg",
-      //     description: "WAISTCOAT WITH CONTRAST PIPING",
-      //     color: "Navy Blue",
-      //     size: "L",
-      //     units: 1,
-      //     amount: 24,
-      //     ref: "3123/619",
-      //   },
-      // ],
-    };
   },
   methods: {
     deleteFromCarts(index) {
